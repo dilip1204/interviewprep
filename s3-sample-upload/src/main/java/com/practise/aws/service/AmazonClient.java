69a8d992391e639044fd003ae20c8728dd00571d
dilip1204 <dilip.kumar1204@gmail.com>
2019-06-11 22:30:28 +0530

    test

--- s3-sample-upload/src/main/java/com/practise/aws/service/AmazonClient.java
+++ s3-sample-upload/src/main/java/com/practise/aws/service/AmazonClient.java
@@ -17,6 +17,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
